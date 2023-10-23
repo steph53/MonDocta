7e26cd5712fa97aad2d4f566f877b3c1758af66e
Stephane Wiston <89261662+steph53@users.noreply.github.com>
2023-10-23 22:24:18 +0200

    prepare for AWS host

--- medusa-monorepo/mondocta/medusa-config.js
+++ medusa-monorepo/mondocta/medusa-config.js
@@ -76,6 +76,7 @@ const projectConfig = {
   store_cors: STORE_CORS,
   database_url: DATABASE_URL,
   admin_cors: ADMIN_CORS,
+  database_type: "postgres",
   // Uncomment the following lines to enable REDIS
   // redis_url: REDIS_URL
 };
