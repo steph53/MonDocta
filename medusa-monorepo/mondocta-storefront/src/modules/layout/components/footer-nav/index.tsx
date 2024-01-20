4a9004823c7cb768663e2f4c08a4aa0c0510cb58
Stephane Wiston <89261662+steph53@users.noreply.github.com>
2024-01-20 12:42:00 +0100

    changed some configurations added a background color

--- medusa-monorepo/mondocta-storefront/src/modules/layout/components/footer-nav/index.tsx
+++ medusa-monorepo/mondocta-storefront/src/modules/layout/components/footer-nav/index.tsx
@@ -10,85 +10,88 @@ const FooterNav = () => {
   const { product_categories } = useProductCategories()
 
   return (
-    <div className=" flex flex-col gap-y-8 pt-16 pb-8 bg-black rounded-t-xl text-white h-[30vh]">
-      <div className="content-container">
-        <div className="content-container flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
-          <div>
-            <Link href="/" className="text-xl-semi uppercase">
-              MonDocta
-            </Link>
-          </div>
-          <div className="text-small-regular grid grid-cols-3 gap-x-10 md:gap-x-16">
-            {product_categories && (
-              <div className="flex flex-col gap-y-2">
-                <span className="text-base-semi">Categories</span>
-                <ul className="grid grid-cols-1 gap-2">
-                  {product_categories?.slice(0, 6).map((c) => {
-                    if (c.parent_category) {
-                      return
-                    }
+    <div className="bg-[#badaf7]">
+      <div className=" flex flex-col gap-y-8 pt-16 pb-8 bg-black rounded-t-xl text-white h-[30vh]">
+        <div className="content-container">
+          <div className="content-container flex flex-col gap-y-6 xsmall:flex-row items-start justify-between">
+            <div>
+              <Link href="/" className="text-xl-semi uppercase">
+                MonDocta
+              </Link>
+            </div>
+            <div className="text-small-regular grid grid-cols-3 gap-x-10 md:gap-x-16">
+              {product_categories && (
+                <div className="flex flex-col gap-y-2">
+                  <span className="text-base-semi">Categories</span>
+                  <ul className="grid grid-cols-1 gap-2">
+                    {product_categories?.slice(0, 6).map((c) => {
+                      if (c.parent_category) {
+                        return
+                      }
 
-                    const children =
-                      c.category_children?.map((child) => ({
-                        name: child.name,
-                        handle: child.handle,
-                        id: child.id,
-                      })) || null
+                      const children =
+                        c.category_children?.map((child) => ({
+                          name: child.name,
+                          handle: child.handle,
+                          id: child.id,
+                        })) || null
 
-                    return (
-                      <li className="flex flex-col gap-2" key={c.id}>
-                        <Link
-                          className={clsx(children && "text-small-semi")}
-                          href={`/${c.handle}`}
-                        >
-                          {c.name}
-                        </Link>
-                        {children && (
-                          <ul className="grid grid-cols-1 ml-3 gap-2">
-                            {children &&
-                              children.map((child) => (
-                                <li key={child.id}>
-                                  <Link href={`/${child.handle}`}>
-                                    {child.name}
-                                  </Link>
-                                </li>
-                              ))}
-                          </ul>
-                        )}
+                      return (
+                        <li className="flex flex-col gap-2" key={c.id}>
+                          <Link
+                            className={clsx(children && "text-small-semi")}
+                            href={`/${c.handle}`}
+                          >
+                            {c.name}
+                          </Link>
+                          {children && (
+                            <ul className="grid grid-cols-1 ml-3 gap-2">
+                              {children &&
+                                children.map((child) => (
+                                  <li key={child.id}>
+                                    <Link href={`/${child.handle}`}>
+                                      {child.name}
+                                    </Link>
+                                  </li>
+                                ))}
+                            </ul>
+                          )}
+                        </li>
+                      )
+                    })}
+                  </ul>
+                </div>
+              )}
+              {collections && (
+                <div className="flex flex-col gap-y-2">
+                  <span className="text-base-semi">Collections</span>
+                  <ul
+                    className={clsx("grid grid-cols-1 gap-2", {
+                      "grid-cols-2": (collections?.length || 0) > 3,
+                    })}
+                  >
+                    {collections?.slice(0, 6).map((c) => (
+                      <li key={c.id}>
+                        <Link href={`/collections/${c.handle}`}>{c.title}</Link>
                       </li>
-                    )
-                  })}
-                </ul>
-              </div>
-            )}
-            {collections && (
-              <div className="flex flex-col gap-y-2">
-                <span className="text-base-semi">Collections</span>
-                <ul
-                  className={clsx("grid grid-cols-1 gap-2", {
-                    "grid-cols-2": (collections?.length || 0) > 3,
-                  })}
-                >
-                  {collections?.slice(0, 6).map((c) => (
-                    <li key={c.id}>
-                      <Link href={`/collections/${c.handle}`}>{c.title}</Link>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
-          <span className="text-xsmall-regular text-gray-500">
-            © Copyright 2022 ACME
-          </span>
-          <div className="min-w-[316px] flex xsmall:justify-end">
-            <CountrySelect />
+          <div className="flex flex-col-reverse gap-y-4 justify-center xsmall:items-center xsmall:flex-row xsmall:items-end xsmall:justify-between">
+            <span className="text-xsmall-regular text-gray-500">
+              © Copyright 2022 ACME
+            </span>
+            <div className="min-w-[316px] flex xsmall:justify-end">
+              <CountrySelect />
+            </div>
           </div>
         </div>
       </div>
     </div>
+    
   )
 }
 
--- medusa-monorepo/mondocta/medusa-config.js
+++ medusa-monorepo/mondocta/medusa-config.js
@@ -23,10 +23,10 @@ try {
 
 // CORS when consuming Medusa from admin
 const ADMIN_CORS =
-  process.env.ADMIN_CORS || "http://localhost:7000,http://localhost:7001";
+  process.env.ADMIN_CORS || "http://admin.mondocta.org:7001,http://localhost:7001,http://localhost:7000,http://admin.mondocta.org:7000";
 
 // CORS to avoid issues when consuming Medusa from a client
-const STORE_CORS = process.env.STORE_CORS || "http://localhost:8000";
+const STORE_CORS = process.env.STORE_CORS || "http://mondocta.org:8000,http://www.mondocta.org:8000,http://localhost:8000";
 
 const DATABASE_URL =
   process.env.DATABASE_URL || "postgres://localhost/medusa-store";
